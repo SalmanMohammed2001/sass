3fb87fc059fe2b76136a068e8ff6ef4b5494f3f5
salman <salmannisthar123@gmail.com>
2024-10-05 15:09:07 +0530

    add hero page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,77 @@
 
-export default function Home() {
+
+import Hero from "@/app/components/hero/hero";
+
+import { getUser } from "./login/users";
+
+
+
+
+
+import { searchProfileData } from "@/app/lib/supabase/profile_update";
+
+
+
+
+
+
+
+export const metadata={
+  title:"Home Page",
+  description:" Home Pafe Details"
+  
+}
+
+
+
+
+
+export default async  function Home() {
+
+
+  const  user= await getUser()   
+  
+
+
+  const userEmail=user?.email;
+  console.log(userEmail != undefined);
+  
+
+
+
+  
+
+
+ 
+   
+   
+   if(userEmail != undefined){
+
+  
+    
+
+    await searchProfileData(userEmail)
+
+
+
+  
+    
+  }
+ 
+
+
+
+
+
+ 
+
+
   return (
-   <div></div>
+
+    <div className="mt-[100px] ">
+
+      <Hero />
+    </div>
+  
   );
 }
