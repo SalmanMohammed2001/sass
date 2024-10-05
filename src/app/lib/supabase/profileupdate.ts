e46078a14fe647dedfeadfa251e7befba7d7d9f4
salman <salmannisthar123@gmail.com>
2024-10-05 15:34:00 +0530

    add hero page

--- src/app/lib/supabase/profileupdate.ts
+++ src/app/lib/supabase/profileupdate.ts
@@ -1,7 +1,7 @@
 "use server"
 
 
-import { createClient } from "./server";
+import { createClient } from '@/app/lib/supabase/server'
 
 const supbase = createClient()
 
@@ -16,22 +16,26 @@ interface Profile {
 
 
 export const searchProfileData=async(email:string)=>{
+
+
+    
+    
     const { data} = await supbase
     .from('profiles')
     .select('*')
     .eq('email', email);
       if(data?.length == 0){
-   
+      await  saveProfileUser(email)
        return;
       }
 
 
       data?.forEach((data:Profile)=>{
         if(data.email == email){
-        
+          return;
         }else{
          
-        
+          saveProfileUser(email)
         }
         
       })      
@@ -40,3 +44,42 @@ export const searchProfileData=async(email:string)=>{
 
 
  
+
+
+  export const saveProfileUser = async ( userEmail:  string) => {
+    const { data, error } = await supbase
+      .from('profiles')
+      .insert([
+        { 
+          email: userEmail 
+        },
+      ])
+      .select();
+  
+    if (data) {
+      subcritionEmail(userEmail);
+    }
+  
+    if (error) {
+      console.error('Error inserting profile:', error);
+      return;
+    }
+  };
+
+
+  export const subcritionEmail = async (userEmail: string) => {
+
+    const { data, error } = await supbase
+      .from('subscriptionData')
+      .insert([
+        { email: userEmail }  
+      ])
+      .select();
+  
+    if (error) {
+      console.error('Error inserting subscription:', error);
+    } else {
+      console.log('Inserted data:', data);
+    }
+  };
+  
