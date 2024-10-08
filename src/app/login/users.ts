99e075282a4fe2a508455bc203415fb8b5964821
salman <salmannisthar123@gmail.com>
2024-10-08 06:46:32 +0530

    add magic link

--- src/app/login/users.ts
+++ src/app/login/users.ts
@@ -4,7 +4,6 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { createClient } from '@/app/lib/supabase/server'
 import { Provider } from '@supabase/supabase-js';
-import { useRouter } from 'next/router';
 
 
 // Define a type for the form data
